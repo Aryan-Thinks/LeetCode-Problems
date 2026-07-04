fcc14bd73493a9bced2b235bac0d5274baa1a89a
Aryan-Thinks <aryanchavan2244@gmail.com>
2026-07-04 23:23:31 +0530

    Cleaned Repo

--- 1148-article-views-i/1148-article-views-i.sql
+++
@@ -1,4 +0,0 @@
--- Write your PostgreSQL query statement below
-SELECT DISTINCT author_id as id
-FROM views
-WHERE author_id = viewer_id;
--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++
@@ -1,4 +0,0 @@
--- Write your PostgreSQL query statement below
-SELECT tweet_id
-FROM tweets
-WHERE LENGTH(content) > 15;
