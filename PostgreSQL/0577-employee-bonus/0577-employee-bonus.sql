e37dee0c99ce0000c212f33e993f6831acd7e93b
Aryan-Thinks <aryanchavan2244@gmail.com>
2026-07-06 23:58:59 +0530

    [Easy] 0577-employee-bonus - 07-06-2026

---
+++ PostgreSQL/0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT name, bonus
+FROM employee as e
+LEFT JOIN bonus as b
+    ON e.empId = b.empId
+WHERE 
+    b.bonus < 1000 
+    OR
+    b.bonus IS NULL;
