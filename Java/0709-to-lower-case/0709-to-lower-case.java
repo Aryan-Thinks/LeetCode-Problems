51cdb29e34f50ffa6f3a865a990404ad23f2b5fb
Aryan-Thinks <aryanchavan2244@gmail.com>
2026-07-10 23:27:27 +0530

    [Easy] 0709-to-lower-case - 07-10-2026

--- Java/0709-to-lower-case/0709-to-lower-case.java
+++ Java/0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,18 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+
+        // Manual
+        char[] ch = s.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] >= 'A' && ch[i] <= 'Z') {
+                ch[i] = (char)(ch[i] + ('a' - 'A')); // add 32
+            }
+        }
+
+        return new String(ch);
+
+        // Using built in method
+        // return s.toLowerCase();
     }
 }
