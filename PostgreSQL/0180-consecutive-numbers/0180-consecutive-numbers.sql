e388414802d8fa9fbc31cf5c5ed9877abe510e72
Aryan-Thinks <aryanchavan2244@gmail.com>
2026-08-20 22:39:36 +0530

    [Medium] 0180-consecutive-numbers - 08-20-2026

---
+++ PostgreSQL/0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+
+SELECT DISTINCT num AS "ConsecutiveNums"
+FROM (
+    SELECT 
+        num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next_num
+    FROM Logs
+) sub
+WHERE num = prev_num 
+  AND num = next_num;
