0ac289e40946cd88ae9226ec47376df743515b51
Aryan-Thinks <aryanchavan2244@gmail.com>
2026-09-22 22:40:51 +0530

    [Medium] 2721-execute-asynchronous-functions-in-parallel - 09-22-2026

---
+++ JavaScript/2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<Array>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        const results = new Array(functions.length);
+        let completed = 0;
+
+        if (functions.length === 0) {
+            resolve([]);
+            return;
+        }
+
+        functions.forEach((fn, index) => {
+            fn()
+                .then(value => {
+                    results[index] = value;
+                    completed++;
+
+                    if (completed === functions.length) {
+                        resolve(results);
+                    }
+                })
+                .catch(reject);
+        });
+    });
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
