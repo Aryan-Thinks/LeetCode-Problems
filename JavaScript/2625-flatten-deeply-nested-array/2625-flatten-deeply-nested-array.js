c8a64ba611333c72cdd92d9f684d29b1cb1da0d9
Aryan-Thinks <aryanchavan2244@gmail.com>
2026-09-24 21:16:49 +0530

    [Medium] 2625-flatten-deeply-nested-array - 09-24-2026

---
+++ JavaScript/2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+
+
+var flat = function (arr, n) {
+    const result = [];
+
+    function flatten(array, depth) {
+        for (const item of array) {
+            if (Array.isArray(item) && depth > 0) {
+                flatten(item, depth - 1);
+            } else {
+                result.push(item);
+            }
+        }
+    }
+
+    flatten(arr, n);
+    return result;
+};
